fd178e5efcb4065402bc1c5d4c82b1a1767a240e
David Su <david.d.su@gmail.com>
2016-11-27 18:19:24 -0500

    add behavior to move forward in direction camera is facing; begin testing mic input

--- Assets/Scripts/AudioBehavior.cs
+++ Assets/Scripts/AudioBehavior.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class AudioBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
-
+		// testing mic input
+		foreach (string device in Microphone.devices) {
+			Debug.Log ("mic: " + device);
+		}
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 }
--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -2,13 +2,25 @@
 using System.Collections;
 
 public class GameBehavior : MonoBehaviour {
+	private GameObject camera;
+	private float vel;
+
 	// Use this for initialization
 	void Start () {
-
+		camera = GameObject.Find ("Main Camera");
+		vel = 0.01f;
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		// move forward in the direction that the camera is facing
+		Transform transform = camera.GetComponent<Transform> ();
+		Vector3 pos = transform.position;
+		Vector3 mvmt = transform.forward * vel;
+		//Quaternion rot = transform.rotation;
+		//Debug.Log (rot);
+		pos.x += mvmt.x;
+		pos.z += mvmt.z;
+		transform.position = pos;
 	}
 }
