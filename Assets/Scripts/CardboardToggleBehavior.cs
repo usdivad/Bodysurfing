5826c56758fd0ff1654f73b718bf31f5e0d7329a
David Su <david.d.su@gmail.com>
2016-12-31 15:17:27 -0500

    add vr toggler

---
+++ Assets/Scripts/CardboardToggleBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardToggleBehavior : MonoBehaviour {
+
+	private bool vrModeEnabled;
+
+	// Use this for initialization
+	void Start () {
+		this.vrModeEnabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Cardboard.SDK.VRModeEnabled = this.vrModeEnabled;
+	}
+
+	public void ToggleVRMode() {
+		this.vrModeEnabled = !this.vrModeEnabled;
+		PlayerPrefs.SetInt ("Vr Mode", this.vrModeEnabled ? 1 : 0);
+	}
+}
---
+++ Assets/Scripts/VRTogglerBehavior.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRTogglerBehavior : MonoBehaviour {
+
+	private bool isGazedAt;
+	private int framesGazedAtThreshold;
+	private int framesGazedAt;
+
+	// Use this for initialization
+	void Start () {
+		this.isGazedAt = false;
+		this.framesGazedAtThreshold = 60;
+		this.framesGazedAt = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isGazedAt) {
+			this.framesGazedAt++;
+		}
+
+		if (this.framesGazedAt > this.framesGazedAtThreshold) {
+			this.ToggleVRMode ();
+			this.framesGazedAt = 0;
+		}
+	}
+
+	public void ToggleVRMode() {
+		GvrViewer.Instance.VRModeEnabled = !GvrViewer.Instance.VRModeEnabled;
+	}
+
+	public void OnGazeEnter() {
+		this.SetGazedAt (true);
+	}
+
+	public void OnGazeExit() {
+		this.SetGazedAt (false);
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		this.isGazedAt = gazedAt;
+		this.framesGazedAt = 0;
+		GetComponent<Renderer>().material.color = gazedAt ? Color.magenta : Color.white;
+	}
+}
